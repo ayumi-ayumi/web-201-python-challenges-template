d659adc2919dd32f8cfad59a4d0a6964f0099c49
Ayumi <ayuayuayuayus@gmail.com>
2022-05-28 15:32:26 +0200

    challenge 4 done

--- test_challenge_04.py
+++ test_challenge_04.py
@@ -6,12 +6,17 @@
 # True or False if the variable is a palindrome.
 
 def is_palindrome(var): #for loop can be used
+
     str_var = str(var) 
     new_var = str_var[::-1] 
-    if (str_var == new_var):
+    
+    if type(new_var) != str:
+        return 'False'
+    elif (str_var == new_var):
         return True
     else:
         return False
+
 
 def test_challenge_04_palindrome_number1():
     assert is_palindrome(545) == True
@@ -20,13 +25,13 @@ def test_challenge_04_palindrome_number2():
     assert is_palindrome(34543) == True
 
 def test_challenge_04_palindrome_number3():
-    assert is_palindrome(345432) == False
+    assert is_palindrome(-343) == False
 
 def test_challenge_04_palindrome_string1():
     assert is_palindrome('MADAM') == True    
     
 def test_challenge_04_palindrome_string2():
-    assert is_palindrome('LOL') == True    
+    assert is_palindrome('MaddaM') == True   
 
-def test_challenge_04_palindrome_string3():
-    assert is_palindrome('MaddaM') == True  
+def test_challenge_04_palindrome_list():
+    assert is_palindrome([34543]) == False    
--- test_challenge_06.py
+++ test_challenge_06.py
@@ -3,9 +3,12 @@
 # sum of the square of all the numbers in the array.
 
 def sum_of_squares(array_of_numbers):
+     if type(array_of_numbers) != list:
+          return 'Not a list'
+
      sum_square = 0
      for i in array_of_numbers:
-          if not isinstance(i, int):
+          if type(i) != int:
                return 'invalid'
           sum_square += i * i
      return sum_square
@@ -19,13 +22,16 @@ def test_challenge_06_happy_case2():
      assert sum_of_squares([0,2,3,4]) == 29
 
 def test_challenge_06_happy_case3(): 
-     assert sum_of_squares([3,4]) == 25
+     assert sum_of_squares([3,-4]) == 25
      
 def test_challenge_06_happy_case4(): 
      assert sum_of_squares([10,20]) == 500
 
 def test_challenge_06_happy_case5(): 
-     assert sum_of_squares([-1,2]) == 5
+     assert sum_of_squares({-1,2}) == "Not a list"
 
-def test_challenge_06_happy_case5(): 
-     assert sum_of_squares(['apple',2]) == "invalid"
+def test_challenge_06_sad_case(): 
+     assert sum_of_squares(1) == "Not a list"
+
+def test_challenge_06_sad_case2(): 
+     assert sum_of_squares(['banana', 'apple']) == "invalid"
