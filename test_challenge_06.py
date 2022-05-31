ff8210d8eb9237941d91de9910a61ced8f9ec7df
Ayumi <ayuayuayuayus@gmail.com>
2022-05-31 15:21:23 +0200

    challenge 6 done

--- test_challenge_06.py
+++ test_challenge_06.py
@@ -8,9 +8,15 @@ def sum_of_squares(array_of_numbers):
 
      sum_square = 0
      for i in array_of_numbers:
-          if type(i) != int or type(i) != float: 
+          # if type(i) != int or type(i) != float:
+          #      return 'invalid'
+          # else:
+          #      sum_square += i * i
+
+          if type(i) == int or type(i) == float:
+               sum_square += i * i
+          else:
                return 'invalid'
-          sum_square += i * i
      return sum_square
 
     # pass
@@ -19,13 +25,13 @@ def test_challenge_06_happy_case():
      assert sum_of_squares([1,2,3,4]) == 30
 
 def test_challenge_06_happy_case2(): 
-     assert sum_of_squares([0,2,3,4]) == 29
+     assert sum_of_squares([0.0,2,3,4.0]) == 29.0
 
 def test_challenge_06_happy_case3(): 
      assert sum_of_squares([3,-4]) == 25
      
 def test_challenge_06_happy_case4(): 
-     assert sum_of_squares([10,20.0]) == 500.0
+     assert sum_of_squares([10,20]) == 500
 
 def test_challenge_06_happy_case5(): 
      assert sum_of_squares({-1,2}) == "Not a list"
