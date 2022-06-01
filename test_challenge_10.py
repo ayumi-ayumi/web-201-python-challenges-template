0a577d6b1edfc8d9a3b05fd12a22880000e466cf
Ayumi <ayuayuayuayus@gmail.com>
2022-06-01 18:37:21 +0200

    challenge 10 done

--- test_challenge_10.py
+++ test_challenge_10.py
@@ -7,20 +7,18 @@
 # sample output: true
 
 def prime(num):
-  if type(num) != int or num < 0:
-    return 'invalid'
-  p = 0
-  for i in range (1,10):
-    if num % i == 0:
-      p += 1
-  if p > 1 or num == 1:
-    # return print("Not a prime number")
-    return False
-  else:
-    # return print('A prime number')
-    return True
-  
-# prime(int(input('number?:')))
+     if type(num) != int or num < 0:
+          return 'invalid'
+     p = 0
+     for i in range (1,10):
+          if num % i == 0:
+               p += 1
+     if p > 1 or num == 1:
+          return False
+     else:
+          return True
+
+
 
 def test_challenge_19_happy_case1(): 
      assert prime(1) == False
--- test_challenge_11.py
+++ test_challenge_11.py
@@ -7,9 +7,14 @@
 
 
 
-first = 'the quick brown fox, jumped over, the mouse.'
-new = first.split(',')
-for i in new[0]:
-  reversed_i = i[::-1]
-  print(reversed_i)
-print(new)
+original = 'the quick brown fox, jumped over, the mouse.'
+original_list = original.split('.')
+original_list_2 = original_list.split(' ')
+
+print(original_list_2)
+
+# new = first.split(',')
+# for i in new[0]:
+#   reversed_i = i[::-1]
+#   print(reversed_i)
+# print(new)
